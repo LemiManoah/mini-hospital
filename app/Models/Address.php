c0bb0bac53f8422f438be48be48111e841ba5e0d
LemiManoah <lionelmanoah@gmail.com>
2026-01-08 15:09:15 +0200

    address crud

---
+++ app/Models/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Address extends Model
+{
+    /** @use HasFactory<\Database\Factories\AddressFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'district',
+        'city',
+        'county',
+        'patient_id',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
